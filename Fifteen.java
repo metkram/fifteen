9f3fa1e5a39726d2493c4b2b9828b0a8dec9f9e5
unknown <metkraming@gmail.com>
2019-04-11 14:56:29 +0300

    add some description and minor improvements

--- Fifteen.java
+++ Fifteen.java
@@ -1,14 +1,14 @@
 public class Fifteen {
 	public static void main(String arg[])
 	throws java.io.IOException {
-		int matrix[][] = {
-			{11, 12, 13, 14},
-				{15, 16, 17, 18},
-					{19, 20, 21, 0},
-						{23, 24, 25, 22}
+		int matrix[][] = { //Matrix where all squares on their places
+			{1, 2, 3, 4},
+			{5, 6, 7, 8},
+			{9, 10, 11, 12},
+			{13, 14, 15, 0}
 		};
-		int zero[] = {0, 0};
-		
+		int zero[] = {0, 0}; //Position of empty square (1/16)
+
 		for(int i = 0; i < matrix.length; i++) {
 			for(int n = 0; n < matrix[i].length; n++) {
 				if (matrix[i][n] == 0) {
@@ -17,11 +17,11 @@ public class Fifteen {
 				}
 			}
 		}
-		
+
 		System.out.println("Zero point at " + (zero[0] + 1) + " line and " + (zero[1] + 1) + " column");
 		for(int i = 0; i < matrix.length; i++) {
 			for(int n = 0; n < matrix[i].length; n++) {
-				System.out.print(matrix[i][n] + " ");
+				System.out.print(((matrix[i][n] < 10) ? "0" : "") + matrix[i][n] + " ");
 			}
 			System.out.println();
 		}
