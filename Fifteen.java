3437fb0cc21be06b88654677afe6e52e76f4278f
unknown <metkraming@gmail.com>
2019-04-30 15:51:54 +0300

    add findZero function

--- Fifteen.java
+++ Fifteen.java
@@ -1,5 +1,18 @@
 import java.util.Arrays;
 public class Fifteen {
+	static int[] findZero(int[][] m) {
+		int[] result = new int[2];
+		for(int i = 0; i < m.length; i++) {
+			for(int n = 0; n < m[i].length; n++) {
+				if (m[i][n] == 0) {
+				result[1] = n;
+				result[0] = i;
+				}
+			}
+		}
+		return result;
+	}
+
 	public static void main(String arg[])
 	throws java.io.IOException {
 		int matrix[][] = { //Matrix where all squares on their places
@@ -8,22 +21,13 @@ public class Fifteen {
 			{9, 10, 11, 12},
 			{13, 14, 15, 0}
 		};
-		int intricateMatrix[][] = { //Matrix where all squares on their places
+		int intricateMatrix[][] = { //Matrix where almost all squares on wrong places
 			{1, 2, 3, 4},
 			{5, 6, 7, 8},
-			{9, 10, 11, 12},
-			{13, 14, 15, 0}
+			{9, 10, 11, 0},
+			{13, 14, 15, 12}
 		};
-		int zero[] = {0, 0}; //Position of empty square (1/16)
-
-		for(int i = 0; i < matrix.length; i++) {
-			for(int n = 0; n < matrix[i].length; n++) {
-				if (matrix[i][n] == 0) {
-				zero[0] = i;
-				zero[1] = n;
-				}
-			}
-		}
+		int zero[] = findZero(intricateMatrix); //Position of empty square (1/16)
 
 		System.out.println("Zero point at " + (zero[0] + 1) + " line and " + (zero[1] + 1) + " column");
 		for(int i = 0; i < matrix.length; i++) {
